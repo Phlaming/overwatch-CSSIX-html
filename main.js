ae1206fa8bd7f7d72873d0cffc8266c4d2d85702
Juan Trinidad <jtrinidad-luis@csumb.edu>
2017-07-26 12:25:37 -0700

    Update

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 //console.log('js file is connected!');
-//console.log(2 + 2);
-
-function checkout (item1, item2, tax, coupon) {
-  var subtotal = item1 + item2;
-  var taxSubtotal = subtotal * (1+ tax);
-  var couponValue = taxSubtotal * coupon;
-  var grandTotal = taxSubtotal - couponValue;
-  return grandTotal;
-  //console.log(grandTotal);
-}
-checkout (100, 200, .05, .25;)
-
-// function helloFunction(name) {
+// //console.log(2 + 2);
+//
+// function checkout (item1, item2, tax, coupon) {
+//   var subtotal = item1 + item2;
+//   var taxSubtotal = subtotal * (1+ tax);
+//   var couponValue = taxSubtotal * coupon;
+//   var grandTotal = taxSubtotal - couponValue;
+//   //return grandTotal;
+//   console.log(grandTotal);
+// }
+// checkout (300, 300, .25, .25);
+//
+// // function helloFunction(name) {
 //   if(name === 'Sammy' {
 //     console.log('Go away. I h8 you.')
 //   } else {
@@ -37,3 +37,18 @@ checkout (100, 200, .05, .25;)
 //
 // var remainAmount = bankAccount - checkout(bananaPrice, chocolatePudding, superCoupon)
 // )
+
+var elevatorLine = [];
+
+function pressFloorNumber(Number) {
+  elevatorLine.push(Number);
+};
+
+pressFloorNumber(0);
+
+function goToNextFloor(){
+  if (elevatorLine.length > 0); {
+      console.log("The line is currently empty");
+
+  }
+}
