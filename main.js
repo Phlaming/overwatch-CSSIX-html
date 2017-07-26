74bacdc1d916c305a96602e6afeec38a24268c72
Juan Trinidad <jtrinidad-luis@csumb.edu>
2017-07-26 09:14:11 -0700

    Update

--- main.js
+++ main.js
@@ -5,11 +5,28 @@ function checkout (item1, item2, coupon) {
   var subtotal = item1 + item2;
   var couponValue = subtotal * coupon;
   var grandTotal = subtotal - couponValue;
-  console.log(grandTotal);
+  return grandTotal;
+  //console.log(grandTotal);
 }
 
-checkout(33, 19, .15);
+function helloFunction(name) {
+  if(name=== 'Sammy' {
+    console.log('Go away. I h8 you.')
+  } else {
+    console.log('Hello ' + name + '!');
+  }
+}
 
-checkout(12, 0, 0);
+helloFunction('sammy');
+
+console.log(checkout(33, 19, .15));
+
+var oneItemResult = checkout(12, 0, 0);
+
+console.log(oneItemResult);
 
 checkout(500, 1000, .05);
+
+var bananaPrice = 1.5;
+var cgicikateOyddubg - 6;
+var superCoupon = .45;
